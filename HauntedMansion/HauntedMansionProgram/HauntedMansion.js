7604c61b5fc2cc451e4797b3f5d59f5198ac9457
MasonLevely <masonlcomputerscience@gmail.com>
2022-01-26 09:36:35 -0800

    Initial Haunted Mansion Commit

---
+++ HauntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -0,0 +1,6 @@
+/*
+Programer: Mason Levely
+Team member: Clayton
+Date: 1-26-2022
+Program: we will be makeing a program that will allow a spherobolt to go through a a haunted mansion were it must make lights and move through obsticles.
+*/
