70aba3e768a03e437f3ec97bfff64db2f99c599e
MasonLevely <masonlcomputerscience@gmail.com>
2022-01-31 09:02:16 -0800

    Blue spot done - Clayton

--- HauntedMansion/HauntedMansionProgram/HauntedMansion.js
+++ HauntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -4,3 +4,16 @@ Team member: Clayton
 Date: 1-26-2022
 Program: we will be makeing a program that will allow a spherobolt to go through a a haunted mansion were it must make lights and move through obsticles.
 */
+
+async function startProgram() {
+// Write code here
+await scrollMatrixText('Start', {r: 66, g: 56, b: 225 }, 60, false);
+// ScrollMatrixText begins the process of scrolling start, the RGB code sets the color, and 60 is the FPS of the scroll
+//false starts the line of code without delay
+await roll(0, 50, 2.8);
+// await roll starts the roll at 0 degrees from start, at 50 speed, for 2.8 seconds
+setMainLed({ r: 0, g: 0, b: 255 }, 10, false);
+//set main led sets the color code to blue
+}
+
+
