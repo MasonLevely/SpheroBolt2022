21e37b9f0be10e64276cec51b89986ef29c1a2a7
MasonLevely <masonlcomputerscience@gmail.com>
2022-02-01 10:00:28 -0800

    End of Day 2/1/2022

--- HauntedMansion/HauntedMansionProgram/HauntedMansion.js
+++ HauntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -1,29 +1,28 @@
-/*
-Programer: Mason Levely
-Team member: Clayton
-Date: 1-26-2022
-Program: we will be makeing a program that will allow a spherobolt to go through a a haunted mansion were it must make lights and move through obsticles.
-*/
-
 async function startProgram() {
 // Write code here
+
+
+
+
+
 await scrollMatrixText('Start', {r: 66, g: 56, b: 225 }, 60, false);
 // ScrollMatrixText begins the process of scrolling start, the RGB code sets the color, and 60 is the FPS of the scroll
 //false starts the line of code without delay
-await roll(0, 50, 2.8);
+await roll(0, 50, 3.3);
 // await roll starts the roll at 0 degrees from start, at 50 speed, for 2.8 seconds
 setMainLed({ r: 0, g: 0, b: 255 }, 10, false);
 //set main led sets the color code to blue
 await roll(45, 50, 1);
 //Makes the sohero go at a 45 degreee angle and go forward
-await roll(130, 50, 1.4);
+await roll(130, 50, 1.6);
 //Changes angle & moves to the sound key
 await Sound.Animal.Chicken.play(true);
-//Plays chicken sound
+//Plays animal sound
 await roll(220, 50, 1);
-//Moves bolt direction to the red dot
+//Moves bolt direction twords the red key
 setMainLed({ r: 255, g: 0, b: 0 }, 10, false);
-await roll(90, 50, 1);
-}
-
+//Sets main Led to Red
+await roll(140, 50, 1.3)
+//Moves Sphero to move twords the next checkpoint
+//await roll(180, 50, 1);
 
